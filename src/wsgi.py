6646f62fbaabdc414394350457b2b9b2df236115
Sam Hill <samuhill@gmail.com>
2024-08-26 16:10:54 -0400

    force http traffic to https

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,7 +1,14 @@
+from flask import redirect, request
 from app import create_app
 
 app = create_app()
 
+@app.before_request
+def before_request():
+    if request.url.startswith('http://'):
+        url = request.url.replace('http://', 'https://', 1)
+        return redirect(url, code=301)
+    
 def handler(event, context):
     from aws_lambda_wsgi import response
     return response(app, event, context)
